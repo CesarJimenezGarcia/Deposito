0b6f2294261fadc3aa8f5f0f039c4be6e0ea906d
belga <belga@Cesar.home>
2023-12-16 20:13:57 +0100

    Actualización con documentación

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,32 +1,74 @@
 package cuentas;
 
+/**
+ * Esto es un comentario JavaDoc de la clase CCuenta
+ */
 public class CCuenta {
 
+	/**
+	 * Campo nombre
+	 */
 	private String nombre;
+	/**
+	 * Campo cuenta
+	 */
 	private String cuenta;
+	/**
+	 * Campo saldo
+	 */
 	private double saldo;
+	/**
+	 * Campo tipoInterés
+	 */
 	private double tipoInterés;
-	
 
+	/**
+	 * Constructor de la clase CCuenta
+	 */
 	public CCuenta() {
 	}
 
+	/**
+	 * Constructor de la clase CCuenta
+	 * 
+	 * @param nom  nombre
+	 * @param cue  cuenta
+	 * @param sal  saldo
+	 * @param tipo tipoInterés
+	 */
 	public CCuenta(String nom, String cue, double sal, double tipo) {
 		setNombre(nom);
 		setCuenta(cue);
 		setSaldo(sal);
 	}
 
+	/**
+	 * Método estado
+	 * 
+	 * @return saldo de la cuenta
+	 */
 	public double estado() {
 		return getSaldo();
 	}
 
+	/**
+	 * Método infresar
+	 * 
+	 * @param cantidad cantidad
+	 * @throws Exception excepción que se genera
+	 */
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		setSaldo(getSaldo() + cantidad);
 	}
 
+	/**
+	 * Método retirar
+	 * 
+	 * @param cantidad cantidad
+	 * @throws Exception excepción que se genera
+	 */
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
 			throw new Exception("No se puede retirar una cantidad negativa");
@@ -35,34 +77,74 @@ public class CCuenta {
 		setSaldo(getSaldo() - cantidad);
 	}
 
+	/**
+	 * Método obtener nombre
+	 * 
+	 * @return nombre nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Método establecer nombre
+	 * 
+	 * @param nombre nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Método para obtener la cuenta
+	 * 
+	 * @return cuenta cuenta
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Método para establecer la cuenta
+	 * 
+	 * @param cuenta cuenta
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Método para obtener el saldo
+	 * 
+	 * @return saldo saldo
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Método para establecer el saldo
+	 * 
+	 * @param saldo saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Método para obtener el tipo de interés
+	 * 
+	 * @return tipoInterés tipo de interés
+	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * Método para establecer el tipo de interés
+	 * 
+	 * @param tipoInterés tipo de interés
+	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -1,9 +1,23 @@
 package cuentas;
 
+/**
+ * Clase principal del programa
+ */
 public class Main {
 
+	/**
+	 * Método main por donde comienza el programa
+	 * 
+	 * @param args argumentos
+	 */
 	public static void main(String[] args) {
+		/**
+		 * Varaible local de tipo Cuenta
+		 */
 		CCuenta cuenta1;
+		/**
+		 * Variable local de tipo double
+		 */
 		double saldoActual;
 
 		cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
@@ -13,6 +27,12 @@ public class Main {
 		operativa_cuenta(cuenta1, 100);
 	}
 
+	/**
+	 * Método para retirar o ingresar dinero a una cuenta
+	 * 
+	 * @param cuenta1  cuenta a la que se retira o ingresa dinero
+	 * @param cantidad cantidad de dinero a retirar o ingresar
+	 */
 	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		try {
 			cuenta1.retirar(2300);
